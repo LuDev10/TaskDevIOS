8b2eb9c923a51c153774092aa8e2a70a887a8cf1
Luis Diaz <diazldev@gmail.com>
2022-06-24 08:27:56 -0300

    Initial Commit

---
+++ TaskDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskDev
+//
+//  Created by LuisDiaz on 24/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
